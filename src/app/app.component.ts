156eb4d03f4c63a7d1c1d66359e681f5a7bf2490
Raymundo Torres <rayplaneta21@gmail.com>
2025-01-20 23:14:47 -0600

    lista nueva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todoapp';
+  bienvenida = 'Bienvenido a la aplicación de tareas';
+  lista = [
+      'Estudiar Angular',
+      'Hacer la compra',
+      'Pasear al perro'
+  ]
 }
