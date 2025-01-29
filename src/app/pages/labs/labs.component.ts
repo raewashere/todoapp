1d76ff2af5ca57f2a060370f254056a0c16d1967
Raymundo Torres <rayplaneta21@gmail.com>
2025-01-28 23:34:26 -0600

    ejemplos del uso de ngIf

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -3,37 +3,38 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-labs',
-  imports : [CommonModule],
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
-  title = "Todoapp";
+  title = 'Todoapp';
   bienvenida = 'Bienvenido a la aplicación de tareas';
   tareas = [
     'Instalar en angular en CLI',
     'Crear proyecto',
     'Crear componentes',
-  ]
+  ];
   name = 'Raymundo';
   //Agregamos una propiedad de edad
   //Si es publica se puede acceder desde el HTML o template
   //Si es privada solo se puede acceder desde el componente
   age = 26;
   disabled = true;
-  img = 'https://www.google.com.mx/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+  img =
+    'https://www.google.com.mx/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
 
   person = {
     name: 'Raymundo',
     age: 26,
-    img: 'https://www.google.com.mx/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
-  }
+    img: 'https://www.google.com.mx/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+  };
 
   //Ejemplo de funcion con alerta
   clickHandler() {
     alert('Le picaste vato');
-  } 
-  
+  }
+
   //Ejemplo de funcion para deteccion de cambios en input
   changeHandler(event: Event) {
     console.log(event);
@@ -63,4 +64,22 @@ export class LabsComponent {
     'Crear proyecto',
     'Crear componentes',
   ]);
+
+  personSignal = signal({
+    name: 'Raymundo',
+    age: 24,
+    img: 'https://www.google.com.mx/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+  });
+
+  changeAgeSignal(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    console.log(newValue);
+    this.personSignal.update((prevState) => {
+      return {
+        ...prevState,
+        age: parseInt(newValue, 10),
+      };
+    });
+  }
 }
