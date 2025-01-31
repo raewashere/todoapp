ad14ba5580bc8f7d7dd50fc1bfd78ce87b9a5fff
Raymundo Torres <rayplaneta21@gmail.com>
2025-01-31 03:50:47 -0600

    Quita debug, valida valores no vacios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,71 +1,86 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 //Importar el modelo de tarea
-import { Task } from '../../models/task.model';  
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   tasks = signal<Task[]>([
     {
       id: Date.now(),
       title: 'Instalar Angular CLI',
-      completed: false
-    }, 
+      completed: false,
+    },
     {
       id: Date.now(),
       title: 'Crear componentes',
-      completed: false
-    }
+      completed: false,
+    },
   ]);
 
   //Agregar una nueva tarea
-  changeHandler(event: Event){
-    const input = event.target as HTMLInputElement;
-    const newTask = input.value;
-    //Agregar nuevo elemento al arreglo sin mutar el arreglo original
-    this.addTask(newTask);
+  changeHandler() {
+    //const input = event.target as HTMLInputElement;
+    //const newTask = input.value;
+
+    if(this.newTaskCtrl.valid){
+      const value = this.newTaskCtrl.value;
+      //Agregar nuevo elemento al arreglo sin mutar el arreglo original
+      if(value !== ''){
+        this.addTask(value);
+        this.newTaskCtrl.setValue('');
+      }
+    }
+    
     //Limpiar el input
-    input.value = '';
+    //input.value = '';
   }
 
   //Agregar tarea a un arreglo de objetos con interfaces
-  addTask(title: string){
+  addTask(title: string) {
     const newTask: Task = {
       id: Date.now(),
       title: title,
-      completed: false
+      completed: false,
     };
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
-
   //Eliminar una tarea de acuerdo a su indice
-  deleteTask(index: number){
+  deleteTask(index: number) {
     //Actualiza el estado de las tareas con una funcion arrow para no mostrar la tarea eliminada
     this.tasks.update((tasks) => tasks.filter((task, i) => i !== index));
   }
 
   //Actualizar el estado de la tarea
-  updateTask(index: number){
+  updateTask(index: number) {
     //Actualizar el estado de la tarea
     this.tasks.update((tasks) => {
       return tasks.map((task, position) => {
-        if(position === index){
+        if (position === index) {
           return {
             ...task,
             //Negar el estado de la tarea
-            completed: !task.completed
+            completed: !task.completed,
           };
         }
         return task;
       });
     });
   }
+
+  newTaskCtrl = new FormControl('', 
+    { nonNullable: true,
+      validators: [
+        Validators.required,
+      ]
+    });
 }
